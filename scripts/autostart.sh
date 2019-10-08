a79ed3787f1df05e16029f686c8b039c2681a2a4
Manuel Cervini <manux999@gmail.com>
2019-10-09 00:39:31 +0200

    added stuff

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./watchConfig.sh &
-conky -b
+#conky -b
 updates=$(apt list --upgradable | grep "Listing" -v | wc -l)
 if [ "$updates" != "0" ];then
 	notify-send -u low $updates" packages with updates"
